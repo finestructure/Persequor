8fe178470f1cdcc5cf760a8c2d627829c38656fe
Sven A. Schmidt <sas@abstracture.de>
2011-09-18 12:38:50 +0200

    Added trac4r

--- Persequor/trac.rb
+++ Persequor/trac.rb
@@ -6,4 +6,22 @@
 #  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
 #
 
+require 'rubygems'
+require 'yaml'
+require 'trac4r/trac'
 
+
+def tickets(trac_url, username, password)
+  trac = Trac.new(trac_url, username, password)
+  trac.tickets.list
+end
+
+
+if __FILE__ == $0
+  CONFIG_FILE = File.expand_path '~/.trac.yml'
+  configuration = YAML.load(File.read(CONFIG_FILE))
+  trac_url = configuration[:trac_url]
+  username = configuration[:username]
+  password = configuration[:password]
+  p tickets(trac_url, username, password)
+end
