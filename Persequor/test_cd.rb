4af150c538fa862bd89f99f43a110d4493736356
Sven A. Schmidt <sas@abstracture.de>
2011-09-20 16:43:51 +0200

    Added test_cd.rb

---
+++ Persequor/test_cd.rb
@@ -0,0 +1,23 @@
+
+require 'test/unit'
+
+framework 'Cocoa'
+
+
+class TestCoreData < Test::Unit::TestCase
+
+  def test_01_predicate
+    predicate = NSPredicate.predicateWithFormat('user == "sas"')
+    assert_not_nil(predicate)
+    assert_equal('user == "sas"', predicate.predicateFormat)
+    # test archiving
+    data = NSKeyedArchiver.archivedDataWithRootObject(predicate)
+    assert_not_nil(data)
+    obj = NSKeyedUnarchiver.unarchiveObjectWithData(data)
+    assert_not_nil(obj)
+    assert_equal('user == "sas"', obj.predicateFormat)
+  end
+
+end
+
+
