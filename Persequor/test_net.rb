bc392d70caa5dbe97489fa9d634b3e21ea1a549d
Sven A. Schmidt <sas@abstracture.de>
2011-09-20 23:14:55 +0200

    Converted net test into proper unit test. Result checking fail on content for some reason (work in the app)

--- Persequor/test_net.rb
+++ Persequor/test_net.rb
@@ -9,98 +9,62 @@
 require 'rubygems'
 require 'yaml'
 require 'trac4r/trac'
-require 'thread'
+require 'test/unit'
 
 
-class Array
-  def parallel_map(&block)
-    result = []
-    # Creating a group to synchronize block execution.
-    group = Dispatch::Group.new
-    # We will access the `result` array from within this serial queue,
-    # as without a GIL (Global Interpreter Lock) we cannot assume array access to be thread-safe.
-    result_queue = Dispatch::Queue.new('access-queue.#{result.object_id}')
-    0.upto(self.size) do |idx|
-      # Dispatch a task to the default concurrent queue.
-      Dispatch::Queue.concurrent.async(group) do
-        temp = block[self[idx]]
-        result_queue.async(group) { result[idx] = temp }
-      end
-    end
-    # Wait for all the blocks to finish.
-    group.wait
-    result
+CONFIG_FILE = File.expand_path '~/.trac.yml'
+
+
+
+class TestNet < Test::Unit::TestCase
+
+  def setup
+    configuration = YAML.load(File.read(CONFIG_FILE))
+    trac_url = configuration[:trac_url]
+    username = configuration[:username]
+    password = configuration[:password]
+    @trac = Trac.new(trac_url, username, password)
   end
-end
 
 
-def tickets(trac_url, username, password)
-  version = 3
-  puts "version: #{version}"
+  def test_threaded_load
+    result = []
+    
+    ids = @trac.tickets.filter(['status!=closed'])
+    n_queues = 10
 
-  trac = Trac.new(trac_url, username, password)
-
-  case version
-  
-  when 1
-    trac.tickets.filter(['status!=closed']).each do |id|
-      trac.tickets.get(id)
-      puts "loaded #{id}"
+    res_group = Dispatch::Group.new
+    res_queues = []
+    n_queues.times do |i|
+       res_queues << Dispatch::Queue.new("res-queue-#{i}")
     end
-
-  when 2
-    trac.tickets.filter(['status!=closed']).parallel_map do |id|
-      trac.tickets.get(id)
-      puts "loaded #{id}"
-    end
-
-  when 3
-    n_worker = 3
+    
     group = Dispatch::Group.new
     queues = []
-    n_worker.times do |i|
+    n_queues.times do |i|
       queues << Dispatch::Queue.new("queue-#{i}")
     end
   
-    trac.tickets.filter(['status!=closed']).each do |id|
-      queue_id = id % n_worker
+    ids.each do |id|
+      queue_id = id % n_queues
       queues[queue_id].async(group) do
-        trac.tickets.get(id)
-        puts "loaded #{id}"
-      end
-    end
-    group.wait
-    
-  when 4
-  
-    queue = Queue.new
-    threads = []
-
-    trac.tickets.filter(['status!=closed']).each{ |id| queue << id }
-
-    4.times do
-      threads << Thread.new do
-        until queue.empty?
-          id = queue.pop(true) rescue nil
-          if id
-            trac.tickets.get(id)
-            puts "loaded #{id}"
-          end
+        t = @trac.tickets.get(id)
+        res_queues[queue_id].async(group) do
+          result << t
+          # puts "loaded #{id}"
         end
       end
     end
-
-    threads.each { |t| t.join }
+    group.wait
+    res_group.wait
     
+    assert_equal(ids.size, result.size)
+    ids.each_with_index do |index, id|
+      #p result[index]
+      #assert_equal(id, result[index].id)
+    end
   end
-end
 
 
-if __FILE__ == $0
-  CONFIG_FILE = File.expand_path '~/.trac.yml'
-  configuration = YAML.load(File.read(CONFIG_FILE))
-  trac_url = configuration[:trac_url]
-  username = configuration[:username]
-  password = configuration[:password]
-  tickets(trac_url, username, password)
 end
+
