2bb366aa68277bb08fc69f68b48142278d71848f
Sven A. Schmidt <sas@abstracture.de>
2011-09-22 14:59:58 +0200

    converted test to use local trac

--- Persequor/test_mytrac.rb
+++ Persequor/test_mytrac.rb
@@ -1,39 +1,42 @@
 require 'rubygems'
-require 'yaml'
 require 'trac4r/trac'
 require 'test/unit'
 require 'time'
 require 'mytrac'
 
-CONFIG_FILE = File.expand_path '~/.trac.yml'
-
+TRAC_URL = "http://localhost:8080"
+USER = "admin"
+PASS = "admin"
 
 
 class TestMyTrac < Test::Unit::TestCase
 
   def setup
-    configuration = YAML.load(File.read(CONFIG_FILE))
-    trac_url = configuration[:trac_url]
-    username = configuration[:username]
-    password = configuration[:password]
-    @trac = Trac.new(trac_url, username, password)
+    @trac = Trac.new(TRAC_URL, USER, PASS)
+  end
+
+  
+  def test_01_ticket_create
+    @trac.tickets.create("test", "description")
+    ids = @trac.tickets.list
+    assert(ids.size > 0)
   end
 
 
-  def test_ticket_changes
+  def test_02_ticket_changes
     since = Time.local(2011,9,1)
     res = @trac.tickets.changes(since)
     assert(res.size > 0)
   end
 
 
-  def test_ticket_settings
+  def test_03_ticket_settings
     assert_not_nil(@trac.tickets.settings)
   end
 
 
-  def test_ticket_desc
-    t = @trac.tickets.get(201)
+  def test_04_ticket_desc
+    t = @trac.tickets.get(1)
     assert_not_nil(t)
     assert_not_nil(t.desc)
     t.desc = 'new description'
