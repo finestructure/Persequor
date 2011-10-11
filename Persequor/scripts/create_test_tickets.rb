67b57329fa5b1e7f7d020a292fb2ef878949e766
Sven A. Schmidt <sas@abstracture.de>
2011-10-11 16:29:09 +0200

    Added create ticket script

---
+++ Persequor/scripts/create_test_tickets.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env macruby
+
+require 'rubygems'
+require 'trac4r/trac'
+
+TRAC_URL = "http://localhost:8080"
+USER = "admin"
+PASS = "admin"
+
+if __FILE__ == $0
+  trac = Trac.new(TRAC_URL, USER, PASS)
+  trac.tickets.list.each{ |id| trac.tickets.delete(id) }
+  (1..100).each{ |i| trac.tickets.create("test #{i}", "description #{i}") }
+end
