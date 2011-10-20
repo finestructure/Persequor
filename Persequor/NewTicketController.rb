1298e804812fedadbcd51d8deece044c81a91b98
Sven A. Schmidt <sas@abstracture.de>
2011-10-20 10:48:43 +0200

    Added user name autocompletion for reporter

--- Persequor/NewTicketController.rb
+++ Persequor/NewTicketController.rb
@@ -35,6 +35,14 @@ class NewTicketController < NSWindowController
     @types.setContent(@document.ticket_cache.types)
     @types.setSelectedObjects(["task"])
     
+    @users = []
+    @document.tickets.arrangedObjects.each do |t|
+      @users << t.reporter
+      @users << t.owner
+      @users << t.cc
+    end
+    @users.uniq!
+    
     @new_ticket = {}
     NSApplication.sharedApplication.beginSheet(
       self.window,
@@ -59,6 +67,7 @@ class NewTicketController < NSWindowController
     case returnCode
     when 0
       # cancel => do nothing
+      puts "new ticket: #{@new_ticket}"
     when 1
       # create ticket
       @new_ticket["component"] = @components.selectedObjects[0]
@@ -67,5 +76,48 @@ class NewTicketController < NSWindowController
       @document.new_ticket(@new_ticket)
     end
   end
+  
+  
+  # text view delegate
+  
+  def control(control, textView:textView, completions:words, 
+    forPartialWordRange:charRange, indexOfSelectedItem:index)
+    # prevent auto-selection
+    #index.assign(-1)
+    typed = textView.string.substringWithRange(charRange)
+    puts "typed: >#{typed}<"
+    if typed != ""
+      completions = @users.find_all{ |i| i.start_with?(typed) }
+    end
+    puts "completions: #{completions}"
+    return completions
+  end
+
+
+  def controlTextDidChange(notification)
+    if @complete_posting
+      return
+    end
+  
+    @complete_posting = true
+  
+    case notification.object.tag
+    when 0 # reporter
+      field_editor = notification.userInfo.objectForKey("NSFieldEditor")
+      text_changed = @last_typed != field_editor.string
+       
+      if text_changed
+        @last_typed = field_editor.string.copy
+        field_editor.complete(nil)
+      end
+    when 1 # owner
+      puts "owner"
+    when 2 # cc
+      puts "cc"
+    end
+    
+    @complete_posting = false
+  end
+
 
 end
